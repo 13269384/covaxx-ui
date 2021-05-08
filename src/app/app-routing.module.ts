53b0611029e0048ad9b71c0ff6eed5da0ed23eaa
Shino Takesako <13269384@student.uts.edu.au>
2021-05-08 21:20:54 +1000

    Commit: Home and booking page created - v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {BookingPageComponent} from './pages/booking-page/booking-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+
+  {
+    path: 'booking',
+    component: BookingPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
